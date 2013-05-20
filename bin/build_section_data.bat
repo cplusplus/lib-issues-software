2987f7f531d2e83d872a5d0c4b40e30aca7a124c
Dani-Hub <daniel.kruegler@googlemail.com>
2013-05-20 13:19:06 +0200

    - Simplify series of grep expressions by pipes - Generate new index/section_data files in bin directory, appending new tr1_section_data after the conversion - Use findstr on Windows (grep does not necessarily exist and/or aren't standardized on these systems)

--- bin/build_section_data.bat
+++ bin/build_section_data.bat
@@ -1,6 +1,9 @@
-grep -v "^[A-Z]" <annex-f >temp1
-grep -v "^[0-9]" <temp1 >temp2
-grep -v "ISO/IEC" <temp2 >temp3
-grep -v "^$" <temp3 >index
-bin\section_data <index >section.data
-ls -l section.data
+@echo off
+REM findstr doesn't interpret a range expression "[A-Z]" in the 
+REM proper way (see "Regex character class ranges [x-y]" from
+REM http://ss64.com/nt/findstr.html), so don't even consider to 
+REM simplify the following:
+findstr /V "^[ABCDEFGHIJKLMNOPQRSTUVWXYZ]" <bin\annex-f | findstr /V "^[0-9]" | findstr /V "ISO/IEC" | findstr /V "^$" >bin\index
+bin\section_data <bin\index >bin\section.data
+type meta-data\tr1_section.data >>bin\section.data
+dir bin\section.data
--- bin/build_section_data.sh
+++ bin/build_section_data.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-grep -v "^[A-Z]" <annex-f >temp1
-grep -v "^[0-9]" <temp1 >temp2
-grep -v "ISO/IEC" <temp2 >temp3
-grep -v '^$' <temp3 >index
-bin/section_data <index >section.data
-ls -l section.data
+grep -v "^[A-Z]" <bin/annex-f | grep -v "^[0-9]" | grep -v "ISO/IEC" | grep -v '^$' >bin/index
+bin/section_data <bin/index >bin/section.data
+cat meta-data/tr1_section.data >>bin/section.data
+ls -l bin/section.data
