7433d4cd160295ed3eff1312cb1ceaee4e855542
Dani-Hub <daniel.kruegler@googlemail.com>
2013-05-20 13:14:22 +0200

    Use -std=c++11 flag instead of -std=c++0x and therefore require at least gcc 4.7

--- bin/build_pgms.sh
+++ bin/build_pgms.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-g++ -m32 -std=c++0x -DNDEBUG -O2 -o bin/lists src/lists.cpp src/date.cpp
-g++ -m32 -std=c++0x -o bin/section_data src/section_data.cpp
-g++ -m32 -std=c++0x -o bin/toc_diff src/toc_diff.cpp
+g++ -m32 -std=c++11 -DNDEBUG -O2 -o bin/lists src/lists.cpp src/date.cpp
+g++ -m32 -std=c++11 -o bin/section_data src/section_data.cpp
+g++ -m32 -std=c++11 -o bin/toc_diff src/toc_diff.cpp
