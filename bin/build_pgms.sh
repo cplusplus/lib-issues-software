b40946b36ef5adfd2fd7a3e1211ca0b8f3a7bfa8
Dani-Hub <daniel.kruegler@googlemail.com>
2014-02-03 22:24:52 +0100

    Try to make .sh file more similar to the behaviour of batch file (hopefully I didn't break anything...)

--- bin/build_pgms.sh
+++ bin/build_pgms.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-g++ -m32 -std=c++11 -DNDEBUG -O2 -o bin/lists src/lists.cpp src/date.cpp
-g++ -m32 -std=c++11 -o bin/section_data src/section_data.cpp
-g++ -m32 -std=c++11 -o bin/toc_diff src/toc_diff.cpp
+echo '"Use -m32 switch to force 32-bit build"'
+g++ $* -std=c++11 -DNDEBUG -O2 -o bin/lists src/lists.cpp src/date.cpp
+g++ $* -std=c++11 -o bin/section_data src/section_data.cpp
+g++ $* -std=c++11 -o bin/toc_diff src/toc_diff.cpp
