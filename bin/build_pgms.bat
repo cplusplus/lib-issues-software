2fa9a61aaa39394094b6e332a1f5bab785c55158
Beman <bdawes@acm.org>
2014-02-02 20:49:44 -0500

    I'm not able to build with -m32 so have switched to allowing arguments from the command line.

--- bin/build_pgms.bat
+++ bin/build_pgms.bat
@@ -1,3 +1,4 @@
-g++ -m32 -std=c++11 -DNDEBUG -O2 -o bin/lists.exe src/lists.cpp src/date.cpp
-g++ -m32 -std=c++11 -o bin/section_data.exe src/section_data.cpp
-g++ -m32 -std=c++11 -o bin/toc_diff.exe src/toc_diff.cpp
+echo "Use -m32 switch to force 32-bit build"
+g++ %* -std=c++11 -DNDEBUG -O2 -o bin/lists.exe src/lists.cpp src/date.cpp
+g++ %* -std=c++11 -o bin/section_data.exe src/section_data.cpp
+g++ %* -std=c++11 -o bin/toc_diff.exe src/toc_diff.cpp
