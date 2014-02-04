0686b7240da852ec7fd0085e2c22d1c818088593
Alisdair Meredith <alisdairm@me.com>
2014-02-04 07:18:37 -0500

    Update build scripts

--- bin/build_pgms.bat
+++ bin/build_pgms.bat
@@ -1,3 +1,3 @@
-g++ -m32 -std=c++11 -DNDEBUG -O2 -o bin/lists.exe src/lists.cpp src/date.cpp
+g++ -m32 -std=c++11 -DNDEBUG -O2 -o bin/lists.exe  src/date.cpp src/issues.cpp src/sections.cpp src/mailing_info.cpp src/report_generator.cpp src/lists.cpp
 g++ -m32 -std=c++11 -o bin/section_data.exe src/section_data.cpp
 g++ -m32 -std=c++11 -o bin/toc_diff.exe src/toc_diff.cpp
--- bin/build_pgms.sh
+++ bin/build_pgms.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-g++ -m32 -std=c++11 -DNDEBUG -O2 -o bin/lists src/lists.cpp src/date.cpp
+g++ -m32 -std=c++11 -DNDEBUG -O2 -o bin/lists src/date.cpp src/issues.cpp src/sections.cpp src/mailing_info.cpp src/report_generator.cpp src/lists.cpp
 g++ -m32 -std=c++11 -o bin/section_data src/section_data.cpp
 g++ -m32 -std=c++11 -o bin/toc_diff src/toc_diff.cpp
