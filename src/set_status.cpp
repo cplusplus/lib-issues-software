75daf291f7bf9ee2335d782c84107bc17de77dd9
Alisdair Meredith <alisdairm@me.com>
2014-02-20 08:56:29 -0500

    Simplified setting status through bash scripts
    The common pattern to move status on a block of issues is now:
       list_issues OLD_STATUS | set_status NEW_STATUS
    This hits a problem when NEW_STATUS has embedded whitespace,
    so the work-around for now is to turn whitespace into under-
    scores, and reverse the transformation inside the set_status
    tool.  This should be possible through appropriate use of
    quotes and similar in bash, but has proved the better of me
    so far, so opting for the quick fix.  Suspect there may be
    similar problems with handling the Windows command line as
    well.

--- src/set_status.cpp
+++ src/set_status.cpp
@@ -1,62 +1,14 @@
-// This program reads all the issues in the issues directory passed as the first command line argument.
-// If all documents are successfully parsed, it will generate the standard LWG Issues List documents
-// for an ISO SC22 WG21 mailing.
-
-// Based on code originally donated by Howard Hinnant
-// Since modified by Alisdair Meredith
-
-// Note that this program requires a reasonably conformant C++11 compiler, supporting at least:
-//    auto
-//    lambda expressions
-//    brace-initialization
-//    range-based for loops
-//    raw string literals
-//    constexpr
-//    new function syntax (late specified return type)
-//    noexcept
-//    new type-alias syntax (using my_name = type)
-
-// Likewise, the following C++11 library facilities are used:
-//    to_string
-//    consistent overloading of 'char const *' and 'std::string'
-
-// Following the planned removal of (deprecated) bool post-increment operator, we now
-// require the following C++14 library facilities:
-//    exchange
-
-// The following C++14 features are being considered for future cleanup/refactoring:
-//    polymorphic lambdas
-//    string UDLs
-
-// The following TS features are also desirable
-//    filesystem
-//    string_view
-
-// Its coding style assumes a standard library optimized with move-semantics
-// The only known compiler to support all of this today is the experimental gcc trunk (4.6)
-
-// TODO
-// .  Better handling of TR "sections", and grouping of issues in "Clause X"
-// .  Sort the Revision comments in the same order as the 'Status' reports, rather than alphabetically
-// .  Lots of tidy and cleanup after merging the revision-generating tool
-// .  Refactor more common text
-// .  Split 'format' function and usage to that the issues vector can pass by const-ref in the common cases
-// .  Document the purpose amd contract on each function
-// Waiting on external fix for preserving file-dates
-// .  sort-by-last-modified-date should offer some filter or separation to see only the issues modified since the last meeting
-
-// Missing standard facilities that we work around
-// . Date
-// . Filesystem navigation
-
-// Missing standard library facilities that would probably not change this program
-// . XML parser
+// This program resets the status attribute of a single issue 
+// It relies entirely on textual search/replace and does not
+// use any other associated functionality of the list management
+// tools.
 
 // standard headers
+#include <algorithm>
 #include <fstream>
-#include <functional>
+//#include <functional>
 #include <iostream>
-#include <iterator>
+//#include <iterator>
 #include <memory>
 #include <sstream>
 #include <stdexcept>
@@ -70,8 +22,8 @@
 #include <sys/types.h>
 
 // solution specific headers
-#include "issues.h"
-#include "sections.h"
+//#include "issues.h"
+//#include "sections.h"
 
 
 #if 0
@@ -121,18 +73,24 @@ void check_is_directory(std::string const & directory) {
    }
 }
 
-int main(int argc, char const* argv[]) {
+int main(int argc, char const * argv[]) {
    try {
       bool trace_on{false};  // Will pick this up from the command line later
 
       if (argc != 3) {
          std::cerr << "Must specify exactly one issue, followed by its new status\n";
+//         for (auto arg : argv) {
+         for (int i{0}; argc != i;  ++i) {
+            char const * arg = argv[i];
+            std::cerr << "\t" << arg << "\n";
+         }
          return -2;
       }
 
       int const issue_number = atoi(argv[1]);
 
-      std::string const new_status{argv[2]};
+      std::string new_status{argv[2]};
+      std::replace(new_status.begin(), new_status.end(), '_', ' ');  // simplifies unix shell scripting
   
       std::string path;
       char cwd[1024];
