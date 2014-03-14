b0cece7dd7073ee3659c70dc7bbc002ccfd0b5f1
Beman <bdawes@acm.org>
2014-03-13 21:08:10 -0400

    Add file_names.cpp to compile.

--- bin/build_pgms.bat
+++ bin/build_pgms.bat
@@ -1,5 +1,5 @@
 echo "Use -m32 switch to force 32-bit build"
-g++ %* -std=c++11 -DNDEBUG -O2 -o bin/lists.exe  src/date.cpp src/issues.cpp src/sections.cpp src/mailing_info.cpp src/report_generator.cpp src/lists.cpp
+g++ %* -std=c++11 -DNDEBUG -O2 -o bin/lists.exe  src/date.cpp src/issues.cpp src/sections.cpp src/mailing_info.cpp src/report_generator.cpp src/file_names.cpp src/lists.cpp
 g++ %* -std=c++11 -o bin/section_data.exe src/section_data.cpp
 g++ %* -std=c++11 -o bin/toc_diff.exe src/toc_diff.cpp
 g++ %* -std=c++11 -DNDEBUG -O2 -o bin/list_issues.exe src/date.cpp src/issues.cpp src/sections.cpp src/list_issues.cpp
--- bin/build_pgms.sh
+++ bin/build_pgms.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo '"Use -m32 switch to force 32-bit build"'
-g++ $* -std=c++11 -DNDEBUG -O2 -o bin/lists src/date.cpp src/issues.cpp src/sections.cpp src/mailing_info.cpp src/report_generator.cpp src/lists.cpp
+g++ $* -std=c++11 -DNDEBUG -O2 -o bin/lists src/date.cpp src/issues.cpp src/sections.cpp src/mailing_info.cpp src/report_generator.cpp src/file_names.cpp src/lists.cpp
 g++ $* -std=c++11 -o bin/section_data src/section_data.cpp
 g++ $* -std=c++11 -o bin/toc_diff src/toc_diff.cpp
 g++ $* -std=c++11 -DNDEBUG -O2 -o bin/list_issues src/date.cpp src/issues.cpp src/sections.cpp src/list_issues.cpp
